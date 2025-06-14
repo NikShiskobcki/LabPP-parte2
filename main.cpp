7e41d6d90e1c3e609023d21b5f4df16dba08b49e
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-06-14 20:58:08 -0300

    Se agrega la opcion 2 en el switch del menu

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ using namespace std;
 int main (int argc, char *argv[]) {
 	usuario usuarios[MAXCANTJUGADORES];
 	apuesta apuestas[20];
-	int pos = 0;
+	int posUsuarios = 0;
+	int posApuestas = 0;
 	
 	int op;
 	printf("**********Bienvenido!**********\n");
@@ -15,7 +16,10 @@ int main (int argc, char *argv[]) {
 		op = menu();
 		switch (op){
 		case 1:
-			gestionUsuarios(usuarios,pos);
+			gestionUsuarios(usuarios,posUsuarios);
+			break;
+		case 2:
+			consultas(usuarios,apuestas,posUsuarios,posApuestas);
 			break;
 		}
 	} while(op!=4);
