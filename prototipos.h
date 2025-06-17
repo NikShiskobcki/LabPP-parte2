56b98fec2755802bfbf994d612ba60bc86fcb8b7
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-06-17 20:07:43 -0300

    Update prototipos.h

--- prototipos.h
+++ prototipos.h
@@ -1,18 +1,32 @@
 #ifndef PROTOTIPOS_H
 #define PROTOTIPOS_H
 int menu();
+
 void gestionUsuarios(usuario usuarios[], int &pos);
+void consultas(usuario usuarios[],int posUsuarios);
+void apostar(usuario usuarios[], int posUsuarios, int &ronda);
+
 void alta(usuario usuarios[],int &pos);
-string ingresoTexto();
-int buscarAlias(string auxAlias, usuario usuarios[],int pos);
-bool ciValida(documento ci, int i);
-documento ingresoCi();
-fecha ingresoFecha();
-bool fechaValida(fecha fAux);
-bool confirmar();
 void modificar(usuario usuarios[],int pos);
-void consultas(usuario usuarios[],apuesta apuestas[],int posUsuarios,int posApuestas);
+
 void listadoJugadores(usuario usuarios[],int pos);
+void listadoApuestasPorJugador(usuario usuarios[], int posUsuarios);
+
+string ingresoTexto();
+fecha ingresoFecha();
+documento ingresoCi();
+
+int buscarAlias(string auxAlias, usuario usuarios[],int pos);
+
+bool ciValida(documento ci, int i);
+bool fechaValida(fecha fAux);
+
+bool confirmar();
+
 void copiarUsuarios(usuario usuarios[],usuario usuariosOrdenados[], int pos);
 void ordenar(usuario usuariosOrdenados[],int pos);
+
+int generarCopa(int i); 
+void mostrarCopas(char c, int copa);
+
 #endif
